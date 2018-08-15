dbf7d8a393225106c2377ab989a48a3f29264964
littlehacker-itcollege <littlehacker.itcollege@gmail.com>
2018-08-15 06:22:58 +0000

    game/urls.py add/

--- game/urls.py
+++ game/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.stage,name='stage'),
-    url(r'^story$',views.story_html,name='story'),
-    url(r'^game$',views.game_html,name='game'),
-    url(r'^head$',views.head_html,name='head'),
+    url(r'^story/$',views.story_html,name='story'),
+    url(r'^game/$',views.game_html,name='game'),
+    url(r'^head/$',views.head_html,name='head'),
     ]
