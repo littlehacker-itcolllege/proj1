8c9e784ac4df6c58a400eef3dde042e2588cfea1
igeishu <17003@std.it-college.ac.jp>
2018-08-15 07:46:46 +0000

    refreshの解決

---
+++ static/js/game.js
@@ -0,0 +1,10 @@
+var $countA = 0;
+function countUpA() {
+    document.getElementById( "sampleOutputA" ).innerHTML = ++$countA;
+    // カウントが20になったら画像の入れ替え
+        if ($countA % 2 == 0) {
+            document.getElementById("img1").src = "../static/game/sample1.gif";
+        } else{
+            document.getElementById("img1").src = "../static/game/sample2.gif";
+        }
+}
