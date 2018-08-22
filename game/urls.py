b30a0d8b082a6871784435045a6dbe0ae813937b
littlehacker-itcollege <littlehacker.itcollege@gmail.com>
2018-08-22 06:03:57 +0000

    stage1-2 add files

--- game/urls.py
+++ game/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     url(r'^stage1-1/game1-1/$',views.game_html,name='game1-1'),
     url(r'^stage1-1/head1-1/$',views.head_html,name='head1-1'),
     url(r'^stage1-1/nextstory1-1/$',views.nextstory_html,name='nextstory1-1'),
+    #stage1-2
+    url(r'^stage1-2/$',views.stage2,name='stage1-2'),
+    url(r'^stage1-2/story1-2/$',views.story_html2,name='story1-2'),
+    url(r'^stage1-2/game1-2/$',views.game_html2,name='game1-2'),
+    url(r'^stage1-2/head1-2/$',views.head_html2,name='head1-2'),
+    url(r'^stage1-2/nextstory1-2/$',views.nextstory_html2,name='nextstory1-2'),
 
     ]
--- game/views.py
+++ game/views.py
@@ -14,4 +14,15 @@ def head_html(request):
     return render(request,'1-1/head1-1.html')
 def nextstory_html(request):
     return render(request,'1-1/nextstory1-1.html')
+#stage1-2
+def stage2(request):
+    return render(request,'1-2/stage1-2.html')
+def story_html2(request):
+    return render(request,'1-2/story1-2.html')
+def game_html2(request):
+    return render(request,'1-2/game1-2.html')
+def head_html2(request):
+    return render(request,'1-2/head1-2.html')
+def nextstory_html2(request):
+    return render(request,'1-2/nextstory1-2.html')
 
